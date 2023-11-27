ec848fcb62ed69f586ec2ab2a2081cdeb9874d04
preciousniphemi <preciousoluwanifemi1999@gmail.com>
2023-11-27 01:19:30 +0100

    post data for multiple companies employees

---
+++ README.md
@@ -0,0 +1,32 @@
+Sure, here's how you can format it as a GitHub README:
+
+---
+
+# LinkedIn Profile Extraction
+
+## Overview
+
+This repository contains scripts and logs for extracting LinkedIn profile posts. The extraction process was run from 11am to 3pm Lagos time.
+
+## Extraction Details
+
+- **Profiles Processed:** The script was initially run on 149 profiles. Each profile took about 10 seconds on average to process.
+- **Pause Time:** To avoid any bans, a pause of around 10-15 minutes was implemented after each profile extraction.
+
+## Stretching the Limits
+
+In an effort to maximize our extraction capabilities, the process was further stretched within the same time frame and run on about 278 profiles. This led to the discovery that a maximum of 200 profiles can be processed within 60 minutes before a restriction is placed on the account.
+
+## Adjustments and Findings
+
+To stay within the limits and avoid restrictions, the script was updated to run on 50 profiles at a time, followed by a pause of 10-15 minutes before continuing the extraction process. This will be further tested when the restriction on my account is lifted.
+
+## Results
+
+In total, posts from 349 profiles were successfully extracted within the 4-hour time frame.
+
+## Future Work
+
+The process will continue to be monitored and adjustments will be made as necessary to ensure as many profiles as possible can be extracted without encountering any restrictions.
+
+---
--- scrape.py
+++ scrape.py
@@ -3,9 +3,17 @@ import requests.cookies
 from linkedin_api import Linkedin
 from requests.cookies import cookiejar_from_dict
 import pandas as pd
+import logging
+import time
 
 def extract_string_after_last_slash(url):
-    return url.rsplit('/', 1)[-1]
+    if "/in" in url:
+        return url.rsplit('/', 1)[-1]
+    else:
+        return ""
+
+# Set up logging
+logging.basicConfig(filename='script.log', level=logging.INFO)
 
 cookies = cookiejar_from_dict(
 {
@@ -17,43 +25,77 @@ cookies = cookiejar_from_dict(
 linkedin = Linkedin("", "", debug=True, cookies=cookies)
 
 # Read the CSV file
-df = pd.read_csv('./test.csv')  # replace 'file.csv' with your CSV file name
+df = pd.read_csv('./Connecteam.csv')  # replace 'file.csv' with your CSV file name
 
 def extract_posts(df):
     # Initialize an empty dictionary to store the results
     results = {}
 
+    # Initialize a counter
+    counter = 0
+
     # Iterate over the column
     for url in df['Profile URl']:  # replace 'column_name' with your column name
         if isinstance(url, str):  # check if url is a string
             username = extract_string_after_last_slash(url)
-            s = linkedin.get_profile_posts(username, post_count=800)
+            if username:
+                try:
+                    start_time = time.time()
+                    s = linkedin.get_profile_posts(username, post_count=800)
+                    end_time = time.time()
+                    elapsed_time = end_time - start_time
+                    logging.info(f"Extracted posts for {username} in {elapsed_time} seconds")
 
-            # Extract the text and url into a separate array
-            posts = []
-            for post in s:
-                if 'commentary' in post:
-                    text = post['commentary']['text']['text']
-                    url = next((action['url'] for action in post['updateMetadata']['updateActions']['actions'] if 'url' in action), None)
-                    if url:
-                        posts.append({'postDetails': text, 'postLink': url})
-                elif 'resharedUpdate' in post and 'commentary' in post['resharedUpdate']:
-                    text = post['resharedUpdate']['commentary']['text']['text']
-                    url = next((action['url'] for action in post['updateMetadata']['updateActions']['actions'] if 'url' in action), None)
-                    if url:
-                        posts.append({'postDetails': text, 'postLink': url})
+                    # Extract the text and url into a separate array
+                    posts = []
+                    for post in s:
+                        if 'commentary' in post:
+                            text = post['commentary']['text']['text']
+                            url = next((action['url'] for action in post['updateMetadata']['updateActions']['actions'] if 'url' in action), None)
+                            if url:
+                                posts.append({'postDetails': text, 'postLink': url})
+                        elif 'resharedUpdate' in post and 'commentary' in post['resharedUpdate']:
+                            text = post['resharedUpdate']['commentary']['text']['text']
+                            url = next((action['url'] for action in post['updateMetadata']['updateActions']['actions'] if 'url' in action), None)
+                            if url:
+                                posts.append({'postDetails': text, 'postLink': url})
 
-            # Add the posts to the results dictionary
-            results[username] = posts
+                    # Add the posts to the results dictionary
+                    results[username] = posts
+
+                    # Increment the counter
+                    counter += 1
+
+                    # If counter reaches 50, pause for 10-15 minutes and reset counter
+                    if counter == 50:
+                        time.sleep(600)  # pause for 10 minutes
+                        counter = 0  # reset counter
+
+                except Exception as e:
+                    logging.error(f"Error occurred while processing {username}: {e}")
+            else:
+                logging.info(f"Skipped {url} due to invalid format")
         else:
-            print(f"Invalid URL: {url}")
+            logging.error(f"Invalid URL: {url}")
 
     # Return the results
     return results
 
-# Call the function and store the results
-results = extract_posts(df)
+# Start the script
+logging.info('Script started')
+start_time = time.time()
 
-# Write the results into a file as JSON
-with open('data.json', 'w') as f:
-    json.dump(results, f)
+# Call the function and store the results
+try:
+    results = extract_posts(df)
+except Exception as e:
+    logging.error(f"Script stopped due to error: {e}")
+finally:
+    # End the script
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    logging.info(f'Script ended, total time: {elapsed_time} seconds')
+
+    # Write the results into a file as JSON
+    with open('data.json', 'w') as f:
+        json.dump(results, f)
